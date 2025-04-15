f3529064755cc285b7170b2fe466e6421bc947eb
yokominn <katherinepapshuova07@gmail.com>
2025-04-15 22:09:04 +0300

    help

---
+++ Assets/Scripts/HandAnimatiobScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Inputs.Readers;
+
+public class HandAnimatiobScript : MonoBehaviour
+{
+    [SerializeField]
+    XRInputValueReader<float> m_TriggerInput;
+
+
+    [SerializeField]
+    XRInputValueReader<float> m_GripInput;
+
+    [SerializeField] Animator animator;
+    private void Update()
+    {
+        animator.SetFloat("Trigger", m_TriggerInput.ReadValue());
+        animator.SetFloat("Grip", m_GripInput.ReadValue());
+
+    }
+}
