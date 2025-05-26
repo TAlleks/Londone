ba6026077ea8f5c4739778beb067e3f6520aaa3d
TAlleks <alexcoruja180@gmail.com>
2025-05-26 13:49:26 +0300

    Death
    украла ноут, теперь у меня раздвоение личности, на самом деле, я делаю игру одна, это просто шиза

--- Assets/Scripts/HP bar/HPbar.cs
+++ Assets/Scripts/HP bar/HPbar.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class HPbar : MonoBehaviour
 {
     [SerializeField] public Image HpBar;
+    [SerializeField] public Animator animatorDoctorWho;
+    [SerializeField] public Collider collider;
+    [SerializeField] public NavMeshAgent agent;
+
     public float HP = 100;
 
     public void ChangeHP(float amount)
     {
         HP += amount;
         HpBar.fillAmount = HP / 100;
+        if (HP <= 0)
+        {
+            animatorDoctorWho.SetTrigger("Death");
+            collider.enabled = false;
+            agent.enabled = false;
+            
+
+        }
+
 
     }
+
+    
 
 }
