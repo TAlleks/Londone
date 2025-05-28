7621017584792fd08c9c41f32e270e896a043b50
TAlleks <alexcoruja180@gmail.com>
2025-05-29 00:01:04 +0300

    ПОБЕДА СЮЖЕТКА ЕСТЬ, МЫ НЕ С ГОЛОЙ ЖОПОЙ, ДАЙ БОГ В ВИАР РАБОТАЕТ

---
+++ Assets/StoryItem.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryItem : MonoBehaviour
+{
+
+    //public GameObject storyCanvas; // Ссылка на Canvas с текстом
+    //public TextMeshProUGUI storyTextUI; // Текстовый элемент UI
+    public GameObject panel;
+
+    private bool isShowing = false;
+
+    public void ShowStory()
+    {
+
+        //storyCanvas.SetActive(true);
+        //storyTextUI.gameObject.SetActive(true);
+        panel.SetActive(true);
+        isShowing = true;
+
+    }
+
+    public void HideStory()
+    {
+
+        //storyCanvas.SetActive(false);
+        //storyTextUI.gameObject.SetActive(false);
+        panel.SetActive(false);
+        isShowing = false;
+
+    }
+}
+
+
